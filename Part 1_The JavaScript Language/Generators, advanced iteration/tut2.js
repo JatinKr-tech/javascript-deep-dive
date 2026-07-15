9c33f16abf7a9f3b3314a71da11b3c3d49c12ef3
Jatin Kumar <woitot9@gmail.com>
2026-07-15 23:55:55 +0530

    docs: 'Async iteration and generators'; feat: 'Completed the lesson added {Real-life example: paginated data';

--- Part 1_The JavaScript Language/Generators, advanced iteration/tut2.js
+++ Part 1_The JavaScript Language/Generators, advanced iteration/tut2.js
@@ -113,6 +113,14 @@ async function* generateSequence(start, end) {
 
 })();
 
+//Under the hood difference
+
+//For async generators, the generator.next() method is asynchronous, it returns promises.
+
+// In a regular generator we’d use result = generator.next() to get values. In an async generator, we should add await, like this:
+
+// result = await generator.next();
+
 //Async iterable range
 
 let range2 = {
@@ -139,6 +147,54 @@ let range2 = {
 
 })();
 
+//
+
+//Please note:
+// Technically, we can add both Symbol.iterator and Symbol.asyncIterator to the object, so it’s both synchronously (for..of) and asynchronously (for await..of) iterable.
+
+// In practice though, that would be a weird thing to do.
+
 //......................................
 
+//Real-life example: paginated data
 
+async function* fetchCommits(repo) {
+  let url = `https://api.github.com/repos/${repo}/commits`;
+
+  while (url) {
+    const response = await fetch(url, { // (1)
+      headers: {'User-Agent': 'Our script'}, // github needs any user-agent header
+    });
+
+    const body = await response.json(); // (2) response is JSON (array of commits)
+
+    // (3) the URL of the next page is in the headers, extract it
+    let nextPage = response.headers.get('Link').match(/<(.*?)>; rel="next"/);
+    nextPage = nextPage?.[1];
+
+    url = nextPage;
+
+    for(let commit of body) { // (4) yield commits one by one, until the page ends
+      yield commit;
+    }
+  }
+}
+
+(async () => {
+
+  let count = 0;
+
+  for await (const commit of fetchCommits('javascript-tutorial/en.javascript.info')) {
+
+    console.log(commit.author.login);
+
+    if (++count == 50) { // let's stop at 100 commits
+      break;
+    }
+  }
+
+})();
+
+//If yield pauses the function, how does it loop through the 30 commits, but then magically "stop" to wait for the next page?
+
+//yield does pauses the generator function (it doesn't loop through automatically through the 30 commits) but actually, for await...of is calling (next method) generator function repeatedly for every yield and that's why it's looping.
